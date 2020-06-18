5c1b72af391ce2c9397502cf28ce824a1ccd2821
Martin <tamarakatic@gmail.com>
2020-06-18 00:46:43 -0400

    create notebook for resutls

--- pure_matching_result.py
+++ pure_matching_result.py
@@ -2,13 +2,15 @@ import os
 import re
 import nltk
 import numpy as np
+import pandas as pd
+from collections import defaultdict
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.metrics import precision_score, recall_score
 from sklearn.metrics import confusion_matrix
 
 
-def find_annotations(openI_files, annotation):
-
+def find_annotations(openI_files, annotation, file_name):
+    results = defaultdict(list)
     labels = []
     for filename in sorted(os.listdir(openI_files), key=lambda x: int(os.path.splitext(x)[0])): # sort files in folder
         with open(openI_files+filename, 'r') as fp:
@@ -18,12 +20,18 @@ def find_annotations(openI_files, annotation):
                     if annotation in line:
                         for nextLine in searchlines[i+1:]:
                             try:
-                                sentence = nextLine.rstrip('\n')
+                                sentence = nextLine.rstrip('\n').rstrip()
                                 key = sentence.split(': ')[1]
-                                # value = sentence.split(': ')[0]
+                                value = sentence.split(': ')[0]
+
+                                results["annotation"].append(value)
+                                results["label"].append(key)
+
                                 labels.append(key)
                             except IndexError:
                                 import pdb; pdb.set_trace()
+    df = pd.DataFrame(results)
+    df.to_csv('/home/martin/Documents/radiology_project/radiology_project/results/labeled/{}.csv'.format(file_name), index=False)
 
     return labels
 
@@ -46,13 +54,13 @@ if __name__ == '__main__':
     root_path = os.path.abspath(os.path.join(current_filepath, os.pardir))
 
     text_openi_file_v1 = os.path.join(root_path, "/home/martin/Documents/radiology_project/radiology_project/data/openITest_Train/test_v1/") 
-    machine_lab_v1 = find_annotations(text_openi_file_v1, "ANNOTATION WITH SENTENCE WITH LABELS")
+    machine_lab_v1 = find_annotations(text_openi_file_v1, "ANNOTATION WITH SENTENCE WITH LABELS", "pure_matching_v1")
 
     text_openi_file_v2 = os.path.join(root_path, "/home/martin/Documents/radiology_project/radiology_project/data/openITest_Train/test_v2/") 
-    machine_lab_v2 = find_annotations(text_openi_file_v2, "ANNOTATION WITH SENTENCE WITH LABELS")
+    machine_lab_v2 = find_annotations(text_openi_file_v2, "ANNOTATION WITH SENTENCE WITH LABELS", "pure_matching_v2")
 
     human_labeled_file = os.path.join(root_path, "/home/martin/Documents/radiology_project/radiology_project/data/openITest_Train/label_100_reports/")
-    human_lab = find_annotations(human_labeled_file, "ANNOTATION WITH SENTENCE LABEL")
+    human_lab = find_annotations(human_labeled_file, "ANNOTATION WITH SENTENCE LABEL", "human_labels")
 
     print("\n ----------- PURE MATCHING ----------- ")  
     print("\n Version 1 (Heading): ")
@@ -62,14 +70,14 @@ if __name__ == '__main__':
     print_results(human_lab, machine_lab_v2)
 
     fastText_v1 = os.path.join(root_path, "/home/martin/Documents/radiology_project/radiology_project/data/openITest_Train/test_fastText_v1/") 
-    machine_lab_fastText_v1 = find_annotations(fastText_v1, "ANNOTATION WITH SENTENCE WITH LABELS")
+    machine_lab_fastText_v1 = find_annotations(fastText_v1, "ANNOTATION WITH SENTENCE WITH LABELS", "fastText_v1")
 
     print("\n ----------- FAST TEXT ----------- ") 
     print("\n Version 1 (Heading): ")
     print_results(human_lab, machine_lab_fastText_v1)
 
     fastText_v2 = os.path.join(root_path, "/home/martin/Documents/radiology_project/radiology_project/data/openITest_Train/test_fastText_v2/") 
-    machine_lab_fastText_v2 = find_annotations(fastText_v2, "ANNOTATION WITH SENTENCE WITH LABELS")
+    machine_lab_fastText_v2 = find_annotations(fastText_v2, "ANNOTATION WITH SENTENCE WITH LABELS", "fastText_v2")
    
     print("\n Version 2 (Heading + Subheadings): ")
     print_results(human_lab, machine_lab_fastText_v2)
